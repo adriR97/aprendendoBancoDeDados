47d9d0574616d3f8e1f9132ba6dbc5bf0aca4eae
Adriane Rodrigues <dryrodrigues2009@gmail.com>
2020-06-17 17:43:06 -0300

    Inserção de dados e alteração do tipo da coluna Cpf

--- Exemplo 1 - Cadastro de Clientes/Modelagem Física.sql
+++ Exemplo 1 - Cadastro de Clientes/Modelagem Física.sql
@@ -21,6 +21,17 @@ SHOW TABLES;
 /*Visualizar informações da tabela*/
 DESC CLIENTE;
 
+/*Mudando tipo da coluna CPF de int para varchar*/
+ALTER TABLE CLIENTE MODIFY CPF VARCHAR(12);
+
+/*Inserindo registros na tabela*/
+INSERT INTO CLIENTE VALUES 
+('Adriane', '123456789-10', 'adriane@gmail.com', '4199999-9999', 'Curitiba - PR', 'F'),
+('Matheus', '987654321-10', 'matheus@gmail.com', '4188888-8888', 'Curitiba - PR', 'M');
+
+/*Ver todos os registros*/
+SELECT * FROM CLIENTE;
+
 
 
 

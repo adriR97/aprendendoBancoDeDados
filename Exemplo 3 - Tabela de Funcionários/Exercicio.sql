c2f4e0ca5c53579779e493774b0fe48d89945623
Adriane Rodrigues <dryrodrigues2009@gmail.com>
2020-06-18 18:04:10 -0300

    Usando update e delete

--- Exemplo 3 - Tabela de Funcionários/Exercicio.sql
+++ Exemplo 3 - Tabela de Funcionários/Exercicio.sql
@@ -15,32 +15,32 @@ insert into funcionarios values
 (1,'Kelley','rkelley0@soundcloud.com','Feminino','Computadores','10/2/2009',67470,'Structural Engineer',2),
 (2,'Armstrong','sarmstrong1@infoseek.co.jp','Masculino','Esporte','3/31/2008',71869,'Financial Advisor',2),
 (3,'Carr','fcarr2@woothemes.com','Masculino','Automotivo','7/12/2009',101768,'Recruiting Manager',3),
-(4,'Murray','jmurray3@gov.uk','Feminino','Joalheria','12/25/2014',96897,'Desktop Support Technician',3),
+(4,'Murray',null,'Feminino','Joalheria','12/25/2014',96897,'Desktop Support Technician',3),
 (5,'Ellis','jellis4@sciencedirect.com','Feminino','Alimentícios','9/19/2002',63702,'Software Engineer III',7),
 (6,'Phillips','bphillips5@time.com','Masculino','Ferramentas','8/21/2013',118497,'Executive Secretary',1),
 (7,'Williamson','rwilliamson6@ted.com','Masculino','Computadores','5/14/2006',65889,'Dental Hygienist',6),
 (8,'Harris','aharris7@ucoz.com','Feminino','Brinquedos','8/12/2003',84427,'Safety Technician I',4),
-(9,'James','rjames8@prnewswire.com','Masculino','Joalheria','9/7/2005',108657,'Sales Associate',2),
+(9,'James',null,'Masculino','Joalheria','9/7/2005',108657,'Sales Associate',2),
 (11,'Jacobs','jjacobsa@sbwire.com','Feminino','Joalheria','11/27/2003',121966,'Community Outreach Specialist',7),
 (12,'Black','mblackb@edublogs.org','Masculino','Roupas','2/4/2003',44179,'Data Coordiator',7),
 (13,'Schmidt','sschmidtc@state.gov','Masculino','Bebês','10/13/2002',85227,'Compensation Analyst',3),
 (14,'Webb','awebbd@baidu.com','Feminino','Computadores','10/22/2006',59763,'Software Test Engineer III',4),
 (15,'Jacobs','ajacobse@google.it','Feminino','Games','3/4/2007',141139,'Community Outreach Specialist',7),
-(16,'Medina','smedinaf@amazonaws.com','Feminino','Bebês','3/14/2008',106659,'Web Developer III',1),
-(17,'Morgan','dmorgang@123-reg.co.uk','Feminino','Crianças','5/4/2011',148952,'Programmer IV',6),
+(16,'Medina',null,'Feminino','Bebês','3/14/2008',106659,'Web Developer III',1),
+(17,'Morgan',null,'Feminino','Crianças','5/4/2011',148952,'Programmer IV',6),
 (18,'Nguyen','jnguyenh@google.com','Masculino','Lar','11/3/2014',93804,'Geologist II',5),
 (19,'Day','rdayi@chronoengine.com','Masculino','Eletronicos','9/22/2004',109890,'VP Sales',3),
 (21,'Bryant','sbryantk@wunderground.com','Feminino','Industrial','8/12/2005',78052,'Software Consultant',3),
 (22,'Alexander','kalexanderl@marketwatch.com','Masculino','Automotivo','12/26/2011',144724,'Marketing Assistant',2),
 (23,'Chapman','jchapmanm@archive.org','Feminino','Joalheria','4/12/2001',126103,'Senior Developer',4),
 (24,'Kelly','ekellyn@weibo.com','Masculino','Jardim','6/29/2006',144965,'Nurse Practicioner',2),
-(25,'Stephens','jstephenso@ow.ly','Masculino','Brinquedos','6/4/2004',70613,'Business Systems Development Analyst',1),
+(25,'Stephens',null,'Masculino','Brinquedos','6/4/2004',70613,'Business Systems Development Analyst',1),
 (26,'Porter','vporterp@yelp.com','Feminino','Lar','5/30/2011',90746,'Dental Hygienist',2),
 (27,'Owens','cowensq@shareasale.com','Feminino','Lar','5/26/2005',78698,'Executive Secretary',2),
 (28,'Scott','cscottr@sphinn.com','Feminino','Books','3/2/2008',63336,'Sales Representative',4),
 (29,'Price','cprices@Brasil.gov','Feminino','Roupas','7/5/2013',95436,'Quality Engineer',3),
 (30,'Weaver','dweavert@shinystat.com','Masculino','Beleza','2/17/2012',83144,'Account Representative III',2),
-(31,'Willis','hwillisu@army.mil','Feminino','Ferramentas','10/21/2012',113507,'Accountant I',7),
+(31,'Willis',null,'Feminino','Ferramentas','10/21/2012',113507,'Accountant I',7),
 (32,'Torres','ltorresv@amazon.de','Masculino','Games','4/7/2014',78245,'Nuclear Power Engineer',6),
 (35,'Smith','bsmithy@statcounter.com','Feminino','Books','6/13/2013',94884,'Librarian',7),
 (36,'Bradley','nbradleyz@goodreads.com','Masculino','Outdoors','12/28/2008',107222,'Payment Adjustment Coordinator',3),
@@ -63,10 +63,9 @@ insert into funcionarios values
 (54,'Reed','sreed1h@intel.com','Feminino','Brinquedos','4/15/2001',84275,'VP Marketing',5),
 (55,'Ross','jross1i@infoseek.co.jp','Masculino','Automotivo','4/21/2004',71363,'Environmental Tech',3),
 (56,'Watkins','kwatkins1j@youtu.be','Masculino','Jardim','12/2/2002',93680,'VP Accounting',3),
-(57,'Allen','jallen1k@google.cn','Feminino','Outdoors','11/20/2000',73755,'Geologist II',3),
+(57,'Allen',null,'Feminino','Outdoors','11/20/2000',73755,'Geologist II',3),
 (58,'Baker','jbaker1l@usnews.com','Masculino','Games','3/2/2007',68857,'Information Systems Manager',2),
 (59,'Garcia','vgarcia1m@deviantart.com','Feminino','Saúde','3/19/2002',48360,'Registered Nurse',6);
-
 
 /*----------MELHORANDO A PERFORMANCE DE PESQUISA----------*/
 /*Sempre escolher a opção que tem mais chances de ser verdadeira quando for OR*/
@@ -87,6 +86,24 @@ select * from funcionarios where (departamento='Lar' and sexo='Feminino') or (de
 
 /*3)Trazer os funcionários do sexo masculino ou os funcionários que trabalhem no Jardim*/
 select * from funcionarios where sexo='Masculino' or departamento='Jardim';
+
+
+/*----------Verificando se os valores são nulos----------*/
+/*Trazendo registros com email nulo*/
+select nome, admissao, cargo from funcionarios where email is null;
+
+/*Trazendo registros não nulos*/
+select nome, admissao, cargo from funcionarios where email is not null;
+
+/*Atualizando um registro*/
+update funcionarios set email='allen@gmail.com' where id=57;
+
+/*Deletando um registro*/
+delete from funcionarios where id=38;
+
+
+
+
 
 
 
